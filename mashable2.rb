be5bbc3dbcd188ae8b2e75f06b27d89d635cf6f1
matrix41 <raymondtam@Raymonds-MacBook-Pro.local>
2014-07-02 09:15:54 -0700

    got the program to successfully print hot titles through web server

--- mashable2.rb
+++ mashable2.rb
@@ -1,4 +1,6 @@
 require 'httparty'
+require 'sinatra'
+
 
 class Mashable2
 
@@ -6,27 +8,39 @@ class Mashable2
     @response ||= HTTParty.get('http://localhost/stories.json')
   end
 
-  def get_title
+
+  def get_new_titles
     battlestar = response.parsed_response
-
     new_articles = battlestar['new']
-    rising_articles = battlestar['rising']
-    hot_articles = battlestar['hot']
 
+    puts "\nNew articles: "
     new_articles.each do |raider|
       puts raider['title']
     end
+  end
 
+
+  def get_rising_titles
+    battlestar = response.parsed_response
+    rising_articles = battlestar['rising']
+
+    puts "\nRising articles: "
     rising_articles.each do |viper|
       puts viper['title']
     end
-
-    hot_articles.each do |raptor|
-    	puts raptor['title']
-    end
-
-
   end
+
+
+  def get_hot_titles( test_array )
+    battlestar = response.parsed_response
+    hot_articles = battlestar['hot']
+
+    puts "\nHot articles: "
+    hot_articles.each do |raptor|
+      test_array << raptor['title']
+    end
+  end
+
 
 end # end class mash2
 
@@ -36,5 +50,13 @@ puts "\nFetching Mashable.com data...."
 mashable_object.response
 puts "Done getting Mashable.com data.\n"
 
-puts "\nGetting title"
-mashable_object.get_title
+# puts "\nGetting title"
+# mashable_object.get_title
+
+test_array = []
+mashable_object.get_hot_titles( test_array )
+
+get '/mashable' do
+  "This is mashable"
+  "#{test_array}"
+end
