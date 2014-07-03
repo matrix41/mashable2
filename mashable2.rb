d35479c7b51928cd0d808d6c80f90a8a87cfc46f
matrix41 <raymondtam@Raymonds-MacBook-Pro.local>
2014-07-03 16:45:24 -0700

    working prototype, finished at 16:45 on 7-3-2014

--- mashable2.rb
+++ mashable2.rb
@@ -4,43 +4,30 @@ require 'sinatra'
 
 class Mashable2
 
-  def response
+  def response 
     @response ||= HTTParty.get('http://localhost/stories.json')
   end
 
 
-  def get_new_titles( test_array )
-    battlestar = response.parsed_response
-    new_articles = battlestar['new']
+  def get_all_data
+    temp_array = []
+    temp_hash ={}
+    hash = response.parsed_response
+#    hash = JSON.parse(response)
+    array = hash['hot']
 
-    puts "\nNew articles: "
-    new_articles.each do |raider|
-      test_array << raider['title']
-    end
-  end
-
-
-  def get_rising_titles( test_array )
-    battlestar = response.parsed_response
-    rising_articles = battlestar['rising']
-
-    puts "\nRising articles: "
-    rising_articles.each do |viper|
-      test_array << viper['title']
-    end
-  end
-
-
-  def get_hot_titles( test_array )
-    battlestar = response.parsed_response
-    hot_articles = battlestar['hot']
-
-    puts "\nHot articles: "
-    hot_articles.each do |raptor|
-      test_array << raptor['title']
-    end
-  end
-
+    array.each do |article|
+#      puts viper[0] # prints new,rising,hot,channel
+      puts article['title']
+      puts article['responsive_images'][2]['size'] # this will return 80x80#
+      temp_hash = { "#{article['title']}" => "#{article['responsive_images'][2]['image']}" }
+      temp_array << temp_hash
+      # temp_array << article['title']
+      # temp_array << article['responsive_images'][2]['image']
+      puts "temp_array.length = #{temp_array.length}"
+    end # end of EACH-DO 
+    return temp_array
+  end # end of get_all_data function 
 
 end # end class mash2
 
@@ -50,46 +37,15 @@ puts "\nFetching Mashable.com data...."
 mashable_object.response
 puts "Done getting Mashable.com data.\n"
 
-# puts "\nGetting title"
-# mashable_object.get_title
+# rising_array = []
+# mashable_object.get_rising_titles( rising_array )
 
-hot_array = []
-mashable_object.get_hot_titles( hot_array )
-
-rising_array = []
-mashable_object.get_rising_titles( rising_array )
-
-new_array = []
-mashable_object.get_new_titles( new_array )
-puts new_array
-
-# hot_string = ''
-# (0..25).each do |i|
-#   hot_string << "#{hot_array[i]}  <br>"
-# end
-# puts "HOT 222"
-# puts hot_string
-# puts " "
-
-
-# new_string = ''
-# (0..25).each do |i|
-#   new_string << "#{new_array[i]}  <br>"
-# end
-# puts "NEW 222"
-# puts new_string
-# puts " "
-
+# rising_array = []
+# puts "rising_arrayA = #{rising_array}"
+title_picture_array = mashable_object.get_all_data
+# puts "rising_arrayB = #{rising_array}"
 
 get '/mashable' do
-#  string = "This is mashable\n\n"
-#  string += "#{test_array}"
-#  string += "\n"
-#  @test_string2 = "#{hot_string}"
-#  @test_string  = "#{new_string}"
-  @my_array = new_array
+  @my_hash = title_picture_array
   erb :mash_view
-
-#  string += "#{test_array[3]}"
-#  string += "\n"
 end
